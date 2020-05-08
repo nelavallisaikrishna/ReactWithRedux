e9f812d688c19d52565a00bfa3f5a470471f3882
nelavallisaikrishna <saikrishna@capitalquotient.com>
2020-05-08 11:48:31 +0530

    basic react render

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ReactWithRedux
+
+To create react app
+### npm  install -g create-react-app 
+### create-react-app  {appName}
+
+OR
+
+### npx create-react-app {appName}
+
+Then it will install all dependencies in project and it will create the setup also (public, src, paxkage.json, .gitignore, yarn.lock)
---
+++ jsx/src/index.js
@@ -0,0 +1,18 @@
+// Import the React and ReactDOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Create a react component
+
+const App = () => {
+    return<div> Hi there </div>
+}
+
+
+//Take the react component and show it on the screen
+
+ReactDOM.render(
+< App / >,
+    document.querySelector('#root')
+)
